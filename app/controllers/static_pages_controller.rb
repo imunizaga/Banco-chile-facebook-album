4235ac349d012cbd2dcc97b99e2d0a72243bc2d7
Ale Almuna <malmuna@csrg.cl>
2012-05-22 18:49:17 -0400

    friends example

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,17 @@ class StaticPagesController < ApplicationController
     end
     begin
       @user = @api.get_object("me")
+      @friends = @api.get_connections('me', 'friends').first(10)
+      @friends_using_app = @api.fql_query("
+        SELECT uid, name, is_app_user, pic_square 
+        FROM user 
+        WHERE uid 
+        IN (
+          SELECT uid2 
+          FROM friend 
+          WHERE uid1 = me()) 
+          AND is_app_user = 1"
+        )
       rescue Exception=>ex
       puts ex.message
     end
