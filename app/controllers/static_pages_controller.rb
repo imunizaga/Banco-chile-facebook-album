d5a1c093936a50733510ba42617b675cd38e7c70
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2012-05-29 19:53:11 -0400

    Static_pages controller removed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,5 @@
 class StaticPagesController < ApplicationController
   def home         
-    if session[:callback_return]  != nil
-        redirect=session[:callback_return]
-        session[:callback_return] = nil
-        redirect_to redirect and return
-    end
-    if session[:access_token] == nil or session['id'] == nil
-      session[:return] = '/static_pages/home'
-      redirect_to '/auth/facebook' and return
-    end
-    @api = Koala::Facebook::API.new(session[:access_token])
-    begin
-      @user = @api.get_object("me")
-      @user['img'] = 'https://graph.facebook.com/' + @user['id'] + '/picture?type=square'
-      rescue Exception=>ex
-      puts ex.message
-    end
   end
 
   def help
