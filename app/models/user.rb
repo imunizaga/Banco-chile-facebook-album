c5c0ce8674f31c7ba1987be79d167f50de9b7c9d
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2012-05-28 22:01:41 -0400

    Missing attributes listed as accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :facebook_id, :twitter_id, :name
+  attr_accessible :email, :facebook_id, :twitter_id, :name, :foursquare_id, :friends, :referals
   has_many :user_cards
   has_many :cards, :through => :user_cards, :uniq => true
 end
