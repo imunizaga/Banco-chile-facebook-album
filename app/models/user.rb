5e42d12d5fe44d96b7af6dff46a756ac4ae72032
Ale Almuna <malmuna@csrg.cl>
2012-05-30 06:11:20 -0400

    Fix ranking

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,18 @@ class User < ActiveRecord::Base
   attr_accessible :email, :facebook_id, :twitter_id, :name, :foursquare_id, :friends, :referals
   has_many :user_cards
   has_many :cards, :through => :user_cards, :uniq => true
-  scope :ordered_by_ncards, order {-cards.count}
 
   def n_cards
     self.cards.count
   end
 
-  def self.ranking
-    ranking = self.ordered_by_ncards.map {|user|{:id=>user.id, :ncards=>user.cards.count}}
+  def self.ranking n=10
+    options = {
+      :include => 'user_cards',
+      :select => 'user_cards.card_id',
+      :group => 'users.id',
+      :order => '1 DESC',
+    }
+    count(options).entries[0..n-1]
   end
 end
