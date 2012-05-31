7eb3ab5fb36835ef2a734e94b8c3163e00e49019
Ale Almuna <malmuna@csrg.cl>
2012-05-31 14:50:29 -0400

    Add album method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,17 @@ class User < ActiveRecord::Base
     self.cards.count
   end
 
+  def album
+    options = {
+      :include=>"user_cards", 
+      :group=>"card_id"
+    }
+    cards= self.user_cards.count(options)
+    user_album = Array.new(Card.count,0)
+    cards.each {|card| user_album[card[0]-1] = card[1]}
+    return user_album
+  end
+
   def self.ranking n=10
     options = {
       :include => 'user_cards',
