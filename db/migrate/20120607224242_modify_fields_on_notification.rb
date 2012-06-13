a8e324d020b3878dcb261cc62d87bebd4a8b10bf
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-06-13 08:10:14 -0400

    Modified the migration "20120607224242_modify_fields_on_notification.rb" to support postgre  by adding to the text columns the line ":limit => nil"

--- db/migrate/20120607224242_modify_fields_on_notification.rb
+++ db/migrate/20120607224242_modify_fields_on_notification.rb
@@ -2,15 +2,15 @@ class ModifyFieldsOnNotification < ActiveRecord::Migration
   def change
     remove_column :notifications, :n_cards
 
-    change_column :notifications, :title, :text
-    change_column :notifications, :description, :text
-    change_column :notifications, :details, :text
+    change_column :notifications, :title, :text, :limit => nil
+    change_column :notifications, :description, :text, :limit => nil
+    change_column :notifications, :details, :text, :limit => nil
 
     add_column :notifications, :user_id, :integer
     add_column :notifications, :receiver_id, :integer
     add_column :notifications, :sender_id, :integer
-    add_column :notifications, :cards_in, :text
-    add_column :notifications, :cards_out, :text
+    add_column :notifications, :cards_in, :text, :limit => nil
+    add_column :notifications, :cards_out, :text, :limit => nil
     add_column :notifications, :status, :integer
     add_column :notifications, :renewal, :date
   end
