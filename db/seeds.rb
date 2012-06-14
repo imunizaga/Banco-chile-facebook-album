b51e65363e2276e5e3ffed2421b48a74db2c66d4
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-06-14 16:04:37 -0400

    New seeds, so, everyone has a trade and receive notification

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,27 +35,38 @@ Challenge.create(n_cards:1, name: 'mail')
 Challenge.create(n_cards:3, name: 'invite')
 Challenge.create(n_cards:5, name: 'invite')
 
-CardPack.create(challenge_id:1)
-UserCard.create(card_pack_id:1, card_id:1, user_id:1)
+# user 1
+CardPack.create(challenge_id:2)
+UserCard.create(card_pack_id:2, card_id:1, user_id:1)
+UserCard.create(card_pack_id:2, card_id:1, user_id:1)
+UserCard.create(card_pack_id:2, card_id:1, user_id:1)
 
 CardPack.create(challenge_id:1)
-UserCard.create(card_pack_id:2, card_id:2, user_id:1)
+UserCard.create(card_pack_id:1, card_id:2, user_id:1)
 
-CardPack.create(challenge_id:1)
-UserCard.create(card_pack_id:3, card_id:3, user_id:2)
+# user 2
+CardPack.create(challenge_id:2)
+UserCard.create(card_pack_id:3, card_id:2, user_id:2)
+UserCard.create(card_pack_id:3, card_id:2, user_id:2)
+UserCard.create(card_pack_id:3, card_id:2, user_id:2)
 
+# user 3
 CardPack.create(challenge_id:3)
 UserCard.create(card_pack_id:4, card_id:1, user_id:3)
 UserCard.create(card_pack_id:4, card_id:2, user_id:3)
-UserCard.create(card_pack_id:4, card_id:2, user_id:3)
+UserCard.create(card_pack_id:4, card_id:3, user_id:3)
 UserCard.create(card_pack_id:4, card_id:3, user_id:3)
 UserCard.create(card_pack_id:4, card_id:3, user_id:3)
 
-CardPack.create(challenge_id:2)
+# user 4
+CardPack.create(challenge_id:3)
+UserCard.create(card_pack_id:5, card_id:4, user_id:4)
+UserCard.create(card_pack_id:5, card_id:4, user_id:4)
 UserCard.create(card_pack_id:5, card_id:4, user_id:4)
 UserCard.create(card_pack_id:5, card_id:4, user_id:4)
 UserCard.create(card_pack_id:5, card_id:4, user_id:4)
 
+# user 5
 CardPack.create(challenge_id:2)
 UserCard.create(card_pack_id:6, card_id:2, user_id:5)
 UserCard.create(card_pack_id:6, card_id:3, user_id:5)
@@ -66,5 +77,14 @@ UserCard.create(card_pack_id:7, card_id:5, user_id:5)
 UserCard.create(card_pack_id:7, card_id:5, user_id:5)
 UserCard.create(card_pack_id:7, card_id:5, user_id:5)
 
-Notification.create(user_id:3, sender_id:4, cards_in:'[4]', cards_out:'[2]')
-Notification.create(user_id:3, description:'retweet', cards_in:'[4]')
+Notification.create(user_id:1, sender_id:2, cards_in:'[2]', cards_out:'[1]')
+Notification.create(user_id:2, sender_id:3, cards_in:'[3]', cards_out:'[2]')
+Notification.create(user_id:3, sender_id:4, cards_in:'[4]', cards_out:'[3]')
+Notification.create(user_id:4, sender_id:5, cards_in:'[5]', cards_out:'[4]')
+Notification.create(user_id:5, sender_id:1, cards_in:'[1]', cards_out:'[5]')
+
+Notification.create(user_id:1, description:'retweet', cards_in:'[1]')
+Notification.create(user_id:2, description:'retweet', cards_in:'[2]')
+Notification.create(user_id:3, description:'retweet', cards_in:'[3]')
+Notification.create(user_id:4, description:'retweet', cards_in:'[4]')
+Notification.create(user_id:5, description:'retweet', cards_in:'[5]')
