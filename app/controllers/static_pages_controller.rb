b4c06d2f947d4c68911be7f71e311a611a97e989
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-07-18 17:27:27 -0400

    fixed a bug that ocurred when the user in the seesion did not belong to the database

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,36 +1,44 @@
 class StaticPagesController < ApplicationController
   def home
     @ranking = User.ranking
+    @user = nil
     if session[:id] != nil
       @user = User.find_by_id(session[:id])
-      @user[:notifications] = @user.notifications
-      @user[:login_status] = 'connected'
+      if @user
+        @user[:notifications] = @user.notifications
+        @user[:login_status] = 'connected'
 
-      # obtain the facebook friends
-      @api = Koala::Facebook::API.new(session[:access_token])
+        # obtain the facebook friends
+        @api = Koala::Facebook::API.new(session[:access_token])
 
-      if @api != nil
-        friends_using_app = @api.fql_query("
-            SELECT uid, name, is_app_user, pic_square
-            FROM user
-            WHERE uid
-            IN (
-              SELECT uid2
-              FROM friend
-              WHERE uid1 = me())
-              AND is_app_user = 1"
-        )
-        # obtain the ruby users
-        friends_fids = friends_using_app.map {|fb_friend| fb_friend['uid'] }
-        friends = User.where(facebook_id: friends_fids)
+        if @api != nil
+          friends_using_app = @api.fql_query("
+              SELECT uid, name, is_app_user, pic_square
+              FROM user
+              WHERE uid
+              IN (
+                SELECT uid2
+                FROM friend
+                WHERE uid1 = me())
+                AND is_app_user = 1"
+          )
+          # obtain the ruby users
+          friends_fids = friends_using_app.map {|fb_friend| fb_friend['uid'] }
+          friends = User.where(facebook_id: friends_fids)
 
-        # adds the friends to the user
-        @user['friends'] = friends
+          # adds the friends to the user
+          @user['friends'] = friends
+        end
+
+        @cards = Card.all
+        @challenges = Challenge.all_without_server_params
+      else
+        session[:id] = nil
       end
+    end
 
-      @cards = Card.all
-      @challenges = Challenge.all_without_server_params
-    else # the user is not logged
+    # if the user was not found
+    if @user == nil
       @user = {}
 
       # we now check weather we got a request
