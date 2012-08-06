afef1f07d4d8cc1f73586fcde0f2f5c5adef0d2a
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-08-06 10:47:17 -0400

    Restored the production SITE_URL value to http://bancochilealbum.herokuapp.com/ to test something

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -6,7 +6,8 @@ if Rails.env == 'development' || Rails.env == 'test'
 else
   APP_ID='127174043311'
   APP_SECRET='0305f7cb7bf84888cc313e9f163ab118'
-  SITE_URL = 'https://apps.facebook.com/127174043311/'
+  #SITE_URL = 'https://apps.facebook.com/127174043311/'
+  SITE_URL = 'http://bancochilealbum.herokuapp.com/'
   ACCESS_TOKEN = '127174043311|F6mjALyN9OCelH8dE1UtPTPl_4k'
 end
 
