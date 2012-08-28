6cdef3c0febef76376600a18b02f795a0712ae63
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-08-28 09:45:53 -0400

    Updated the album like challenge

---
+++ db/migrate/20120828134108_change_like_album_challenge.rb
@@ -0,0 +1,14 @@
+class ChangeLikeAlbumChallenge < ActiveRecord::Migration
+  def up
+    begin
+      like_challenge = Challenge.find(3)
+      like_challenge.client_param = 'http://apps.facebook.com/elequipodetodos/'
+      like_challenge.server_param = '446490652062870',
+      like_challenge.save()
+    rescue Exception
+    end
+  end
+
+  def down
+  end
+end
